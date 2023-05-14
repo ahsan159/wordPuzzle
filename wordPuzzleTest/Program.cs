dc86cc18239da387394e56f023884a84ff29ad5c
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-05-14 17:22:21 +0500

    tested using console app. WPF in progress.

--- wordPuzzleTest/Program.cs
+++ wordPuzzleTest/Program.cs
@@ -15,33 +15,40 @@ namespace Program
             {
                 // Console.WriteLine(engine.getWord());
                 string[] str = engine.getPuzzle();
-                Console.WriteLine(str[0]+","+str[1]);
+                Console.WriteLine(str[0] + "," + str[1]);
             }
 
             bool continueGame = true;
-            while(continueGame)
+            while (continueGame)
             {
                 string[] str = engine.getPuzzle();
                 Console.WriteLine(str[0]);
                 string puzzle = str[1];
                 Console.WriteLine(puzzle);
-                do 
+                do
                 {
                     Console.Write(puzzle + " Enter Key: ");
                     char guess = char.Parse(Console.ReadLine());
                     int index = puzzle.IndexOf('_');
-                    puzzle = puzzle.Remove(index,1);
-                    puzzle = puzzle.Insert(index,guess.ToString());
+                    try
+                    {
+                        puzzle = puzzle.Remove(index, 1);
+                        puzzle = puzzle.Insert(index, guess.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
                     Console.WriteLine(puzzle);
-                }   while(!engine.checkPuzzle(puzzle));
-                Console.WriteLine("Continue??");
+                } while (!engine.checkPuzzle(puzzle));
+                Console.WriteLine("Continue?? (Press 0 to exit)");
                 int i = int.Parse(Console.ReadLine());
                 if (i == 0)
                 {
                     continueGame = false;
                 }
             }
-        }        
-        
+        }
+
     }
 }
