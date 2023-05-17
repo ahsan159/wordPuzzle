9432171b2d721eaaade258ac1ed169ef33738e15
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2023-05-17 12:32:28 +0500

    Initial GUI prepared

--- wordPuzzleEngine/Class1.cs
+++ wordPuzzleEngine/Class1.cs
@@ -1,139 +1,141 @@
 
 namespace wordPuzzle;
 
-    public enum puzzleStatus
+public enum puzzleStatus
+{
+    Unintialized,
+    Initialized,
+    Success,
+    BetterLuckNextTime,
+    Puzzle
+}
+public enum puzzleLevel
+{
+    Novice,
+    Beginner,
+    Intermadiate,
+    Expert
+}
+public class wordPuzzleEngine
+{
+    // private int chances = 0;
+    private puzzleStatus status = puzzleStatus.Unintialized;
+    private int attempt = 6;
+    private List<string> words = new List<string>();
+    private string lastWord = "";
+    private string lastPuzzle = "";
+    //private Timer timer; future use
+    public int max = -1;
+    Random rnd = new Random();
+    //puzzleLevel level = puzzleLevel.Beginner;
+    int maxWordLength = 4;
+    int missingChars = 1;
+    string message = "initiate New";
+    public wordPuzzleEngine()
     {
-        Unintialized,
-        Initialized,
-        Success,
-        BetterLuckNextTime,
-        Puzzle
+        Console.WriteLine("Engine Constructor");
+        words.Clear();
+        loadDictionary("./oeDictionaryWords.txt");
+        max = words.Count;
+        //level = puzzleLevel.Novice;
+        maxWordLength = 10;
+        missingChars = 3;
+        status = puzzleStatus.Initialized;
+        //timer = new Timer(getPuzzle);
     }
-    public enum puzzleLevel
+    ~wordPuzzleEngine()
     {
-        Novice,
-        Beginner,
-        Intermadiate,
-        Expert
+        Console.WriteLine("Engine Destructor");
     }
-    public class wordPuzzleEngine
+    public void setLevel(puzzleLevel lvl)
     {
-        // private int chances = 0;
-        private puzzleStatus status = puzzleStatus.Unintialized;
-        private int attempt = 6;
-        private List<string> words = new List<string>();
-        private string lastWord = "";
-        private string lastPuzzle = "";
-        //private Timer timer; future use
-        public int max = -1;
-        Random rnd = new Random();
-        //puzzleLevel level = puzzleLevel.Beginner;
-        int maxWordLength = 4;        
-        int missingChars = 1;
-        string message = "initiate New";
-        public wordPuzzleEngine()
+        if (lvl == puzzleLevel.Novice)
         {
-            Console.WriteLine("Engine Constructor");
-            words.Clear();
-            loadDictionary("./oeDictionaryWords.txt");
-            max = words.Count;
-            //level = puzzleLevel.Novice;
-            maxWordLength = 10;
+            maxWordLength = 4;
+            missingChars = 1;
+            attempt = 3;
+        }
+        else if (lvl == puzzleLevel.Beginner)
+        {
+            maxWordLength = 6;
             missingChars = 3;
-            status = puzzleStatus.Initialized;
-            //timer = new Timer(getPuzzle);
+            attempt = 5;
         }
-        ~wordPuzzleEngine()
+        else if (lvl == puzzleLevel.Intermadiate)
         {
-            Console.WriteLine("Engine Destructor");
+            maxWordLength = 10;
+            missingChars = 4;
+            attempt = 6;
         }
-        public void setLevel(puzzleLevel lvl)
+        else if (lvl == puzzleLevel.Expert)
         {
-            if (lvl == puzzleLevel.Novice)
-            {
-                maxWordLength = 4;
-                missingChars = 1;
-                attempt = 3;
-            }
-            else if (lvl == puzzleLevel.Beginner)
-            {
-                maxWordLength = 6;
-                missingChars = 3;
-                attempt = 5;
-            }
-            else if (lvl == puzzleLevel.Intermadiate)
-            {
-                maxWordLength = 10;
-                missingChars = 4;
-                attempt = 6;
-            }
-            else if (lvl == puzzleLevel.Expert)
-            {
-                maxWordLength = 15;
-                missingChars = 5;
-                attempt = 7;
-            }
-            status = puzzleStatus.Initialized;
+            maxWordLength = 15;
+            missingChars = 5;
+            attempt = 7;
         }
-        public void loadDictionary(string str)
+        status = puzzleStatus.Initialized;
+    }
+    public void loadDictionary(string str)
+    {
+        words.Clear();
+        string[] strWords = File.ReadAllLines(str);
+        foreach (string s in strWords)
         {
-            words.Clear();
-            string[] strWords = File.ReadAllLines(str);
-            foreach (string s in strWords)
-            {
-                words.Add(s);
-            }
+            words.Add(s);
         }
+    }
 
-        public string getWord()
+    public string getWord()
+    {
+        int length = 100;
+        string retWord = "";
+        while (length > maxWordLength)
         {
-            int length = 100;
-            string retWord = "";
-            while (length > maxWordLength)
-            {
-                int wInt = rnd.Next(max);
-                retWord = words.ElementAt(wInt);
-                length = retWord.Length;
-            }
-            lastWord = retWord;
-            return retWord;
+            int wInt = rnd.Next(max);
+            retWord = words.ElementAt(wInt);
+            length = retWord.Length;
         }
-        public string[] getPuzzle()
+        lastWord = retWord;
+        return retWord;
+    }
+    public string[] getPuzzle()
+    {
+        string wordCorrect = getWord().ToUpper();
+        string[] retPuzzle = new string[2];
+        int minMissingChars = missingChars < wordCorrect.Length ? missingChars : wordCorrect.Length - 1;
+        int[] missingPositions = new int[minMissingChars];
+        string wordPuzzle = wordCorrect;
+        retPuzzle[0] = wordCorrect;
+        int wordLen = wordCorrect.Length;
+        bool distinct = false;
+        while (!distinct)
         {
-            string wordCorrect = getWord().ToUpper();
-            string[] retPuzzle = new string[2];
-            int minMissingChars = missingChars<wordCorrect.Length?missingChars:wordCorrect.Length-1;
-            int[] missingPositions = new int[minMissingChars];
-            string wordPuzzle = wordCorrect;
-            retPuzzle[0] = wordCorrect;
-            int wordLen = wordCorrect.Length;
-            bool distinct = false;
-            while (!distinct)
+            for (int i = 0; i < minMissingChars; i++)
             {
-                for (int i = 0; i < minMissingChars; i++)
-                {
-                    missingPositions[i] = rnd.Next(wordLen);
-                }
-                if (missingPositions.Distinct().Count() == missingPositions.Count())
-                {
-                    distinct = true;
-                }
+                missingPositions[i] = rnd.Next(wordLen);
             }
-            foreach (int i in missingPositions)
+            if (missingPositions.Distinct().Count() == missingPositions.Count())
             {
-                wordPuzzle = wordPuzzle.Remove(i,1);
-                wordPuzzle = wordPuzzle.Insert(i,"_");
+                distinct = true;
             }
-            retPuzzle[1] = wordPuzzle;
-            lastWord = wordCorrect;
-            lastPuzzle = wordPuzzle;
-            // chances = 0;
-            attempt = missingChars + 2;
-            message = "Attempts Left: " + attempt.ToString();
-            status = puzzleStatus.Puzzle;
-            return retPuzzle;
-        }        
-        public bool checkPuzzle(string inWord)
+        }
+        foreach (int i in missingPositions)
+        {
+            wordPuzzle = wordPuzzle.Remove(i, 1);
+            wordPuzzle = wordPuzzle.Insert(i, "_");
+        }
+        retPuzzle[1] = wordPuzzle;
+        lastWord = wordCorrect;
+        lastPuzzle = wordPuzzle;
+        // chances = 0;
+        attempt = missingChars + 2;
+        message = "Attempts Left: " + attempt.ToString();
+        status = puzzleStatus.Puzzle;
+        return retPuzzle;
+    }
+    public bool checkPuzzle(string inWord)
+    {
+        if (status == puzzleStatus.Puzzle)
         {
             if (attempt-- == 0)
             {
@@ -146,19 +148,20 @@ namespace wordPuzzle;
                 status = puzzleStatus.Success;
                 return true;
             }
-            //status = puzzleStatus.BetterLuckNextTime;
-            return false;
         }
-        public puzzleStatus getStatus()
-        {
-            return status;
-        }
-        public string getMessage()
-        {
-            return message;
-        }
-        public void setSeed(int i)
-        {
-            rnd = new Random(i);
-        }
+        //status = puzzleStatus.BetterLuckNextTime;
+        return false;
     }
+    public puzzleStatus getStatus()
+    {
+        return status;
+    }
+    public string getMessage()
+    {
+        return message;
+    }
+    public void setSeed(int i)
+    {
+        rnd = new Random(i);
+    }
+}
---
+++ wordPuzzleGUI/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wordPuzzle;
+namespace wordPuzzleGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private wordPuzzleEngine engine;
+        TextBox[] letterBox;
+        public MainWindow()
+        {
+            engine = new wordPuzzleEngine();
+            InitializeComponent();
+            //MessageBox.Show("Ahsan");
+        }
+
+        private void getPuzzle_Click(object sender, RoutedEventArgs e)
+        {
+            //string[] str = engine.getPuzzle();
+            //wordText.Text = str[0];
+            // puzzleText.Text = str[1];
+            // MessageBox.Show("Ahsan");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {            
+            string[] str = engine.getPuzzle();
+            wordText2.Text = str[0];
+            //puzzleText2.Text = str[1];
+            double totalLetters = (double)str[0].Length;
+            letterBox = new TextBox[(int)totalLetters];            
+            double boxWidth = (puzzlePanel.Width - 2*5*totalLetters)/totalLetters;
+            puzzlePanel.Children.Clear();
+            for(int i = 0; i < letterBox.Length; i++)
+            {
+                letterBox[i] = new TextBox();
+                letterBox[i].Width = (int)boxWidth;
+                letterBox[i].Margin = new Thickness(5,0,5,0);
+                letterBox[i].CharacterCasing = CharacterCasing.Upper;
+                 if(str[1].ElementAt(i)!='_')
+                 {
+                    letterBox[i].Text = str[1].ElementAt(i).ToString();
+                 }
+                 letterBox[i].TextChanged += textChangeFunction;
+                puzzlePanel.Children.Add(letterBox[i]);
+            }
+            //MessageBox.Show(str[0]);
+        }
+        private void textChangeFunction(object sender, RoutedEventArgs args)
+        {
+            string str = string.Empty;
+            foreach(TextBox tb in letterBox)
+            {
+                //tb.Text = tb.Text.ToUpper();
+                if (tb.Text.Equals(string.Empty) || tb.Text.Equals(" "))
+                {
+                    str = str + "_";
+                }
+                else 
+                {
+                    str = str + tb.Text.ToUpper();
+                }
+            }
+            //MessageBox.Show(str);
+            engine.checkPuzzle(str);
+            puzzleStatus status = engine.getStatus();
+            if (status == puzzleStatus.Success)
+            {
+                MessageBox.Show("WOW!!");
+            }
+            else if (status == puzzleStatus.BetterLuckNextTime)
+            {
+                MessageBox.Show("Better Luck Next Time");
+            }
+        }
+    }    
+}
